118df0f69d709e8b09ed0e0ec40737781555b2e2
Gabriel <gabriel3_lipe@hotmail.com>
2025-04-16 17:51:51 +0000

    conclui desafio novato batalha batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int matriz[10][10];//declarei/criei a matriz.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            matriz[i][j] = 0; //atribui o valor zero a todos os 100 espaços da matriz.
+        }
+    }
+//_______________________________________//
+    for (int j = 1; j < 4; j++){
+        matriz[8][j] = 3;//colocando o segundo navio na linha 8, colunas 1, 2 e 3 com valor 3.
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    for (int i = 6; i < 9; i++){
+            matriz[i][9] = 3; //colocando o primeiro navio na coluna 9, linhas 6,7 e 8 com valor 3.
+        }
+//________________________________________//
+        printf("  ");
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    for (char linhacabecario = 'A'; linhacabecario <= 'J'; linhacabecario++){
+            printf(" %c ", linhacabecario);//imprimindo o cabeçario na horizontal (LETRAS)
+        }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+        printf("\n");
+//______________________________________//
+    for (int i = 0; i < 10; i++){
+            printf("%2d", i + 1); //imprime o cabecario na vertical (numeros)
+        for (int j = 0; j < 10; j++)
+        {
+            printf(" %d ", matriz[i][j]);//Imprime  o valor da matriz linha por linha.
+        }
+        printf("\n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        }
+        
 
-    return 0;
-}
+        return 0;
+
+    }
+    
+
+
+
+
+
+
+
+
+
+    
+
